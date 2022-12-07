fdf3f748f1a2c2b106812108cb1908c69327665d
smdewi <smdewi2020@gmail.com>
2022-12-07 20:43:37 +0800

    Display menu items in simple list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-// import Menu from './Menu';
-import MallDirectory from './MallDirectory';
+import Menu from './Menu';
+// import MallDirectory from './MallDirectory';
 
 function App() {
   return (
     <div className="app-container">
-      <MallDirectory />
+      <Menu />
     </div>
   );
 }
--- src/Menu.js
+++ src/Menu.js
@@ -2,13 +2,27 @@ import React from 'react';
 
 export default class Menu extends React.Component {
     state = {
-        
+        menuItems: [
+          "Egg Tarts",
+          "Chicken Rice",
+          "Penang Rojak",
+          "Steamed Chicken Noodles"
+        ]
+    }
+
+    showMenu() {
+      let jsx = [];
+      for (let item of this.state.menuItems) {
+        jsx.push(<li key={item}>{item}</li>)
+      }
+      return jsx;
     }
 
     render() {
         return (
             <React.Fragment>
                 <h1>Menu </h1>
+                {this.showMenu()}
             </React.Fragment>
         );
     }
